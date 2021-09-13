ce60591d5e81840e23dc0edab768a8c726cafbb1
ranjan giri <ranjan0r890@gmail.com>
2021-09-13 14:23:31 +0530

    set up and runnig the port

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const connect = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/collegeDataBase', {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
 
-
+    })
+}
+app.listen(3001, async (req,res) => {
+    await connect();
+    console.log("port is running")
+})
 
 
