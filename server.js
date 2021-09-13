0a18e99d57d43dbeba04965120ba49331141c51b
ranjan giri <ranjan0r890@gmail.com>
2021-09-13 14:41:37 +0530

    created student schema

--- server.js
+++ server.js
@@ -10,6 +10,39 @@ const connect = () => {
 
     })
 }
+app.use(express.json())
+//student schema
+const studentSchema = new mongoose.Schema({
+    first_Name: { type: String, requred: true },
+    last_Name: { type: String, requred: true },
+    age: { type: Number, requred: true },
+    gender:{ type: String, requred: true },
+    course_name:{type:String,requred:true},
+})
+//creating model for schema
+const Student = mongoose.model('students', studentSchema);
+
+app.post('/students', async (req, res) => {
+    try {
+        const user = await Student.create(req.body)
+        console.log(user);
+    }
+    catch (err) {
+        res.send("error ocured");
+    }
+})
+app.get("/students", async (req, res) => {
+    try {
+        const user = await Student.find().lean().exec()
+        
+    }
+    catch {
+        console.log("error");
+        
+    }
+})
+
+
 app.listen(3001, async (req,res) => {
     await connect();
     console.log("port is running")
